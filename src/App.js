81da69848fc46e83af573d3ab2a9909755b803e0
techay-mihir-simform <techaypatel123@gmail.com>
2022-04-13 09:42:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>naya app 3 src/App.js</code> and save to reload.
+          Edit <code> new code 2 src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
